dd67978362f2a8bfb62c878e77b60a4d7c88c3ed
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2024-05-31 17:08:49 -0300

    /

--- Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/modelos/Palabra.java
+++ Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/modelos/Palabra.java
@@ -9,5 +9,28 @@ package com.ejercicio5.ejercicio_5.modelos;
  * @author Baltasar
  */
 public class Palabra {
+    private String palabra;
+    private int cantidadVeces;
+
+    public Palabra(String palabra, int cantidadVeces) {
+        this.palabra = palabra;
+        this.cantidadVeces = cantidadVeces;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public int getCantidadVeces() {
+        return cantidadVeces;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public void setCantidadVeces(int cantidadVeces) {
+        this.cantidadVeces = cantidadVeces;
+    }
     
 }
