9752702f2967b540f5442945257ce71c932d8799
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2024-05-31 16:09:48 -0300

    -

---
+++ Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/modelos/Palabra.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejercicio5.ejercicio_5.modelos;
+
+/**
+ *
+ * @author Baltasar
+ */
+public class Palabra {
+    
+}
