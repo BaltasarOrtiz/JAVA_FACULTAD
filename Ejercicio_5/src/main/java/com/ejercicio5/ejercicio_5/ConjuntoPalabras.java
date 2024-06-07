3acc6500b34e022a99ab74d86fdc9bb6f42dbc83
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2024-06-06 22:03:49 -0300

    Ejercicio 5 Terminado

---
+++ Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/ConjuntoPalabras.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejercicio5.ejercicio_5;
+import com.ejercicio5.ejercicio_5.modelos.Palabra;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Baltasar
+ */
+public class ConjuntoPalabras {
+    List<Palabra> palabras;
+
+    public ConjuntoPalabras() {
+        palabras = new ArrayList<>();
+    }
+
+    //syncronized
+    public void agregarPalabra(Palabra p){
+        palabras.add(p);
+    }
+    
+    public String buscarPalabra(String PalabraBuscada){
+        for (Palabra palabra : palabras) {
+            if (palabra.getPalabra().equals(PalabraBuscada)) {
+                return palabra.toString();
+            }
+        }
+        return "No se encontro la palabra";
+    }
+
+    public void mostrar() {
+        System.out.println("-------------------------------");
+        System.out.println("Lista de palabras y frecuencias");
+        System.out.println("-------------------------------");
+        for (Palabra palabra : palabras) {
+            System.out.printf("Palabra: %-20s Frecuencia: %d%n", palabra.getPalabra(), palabra.getCantidadVeces());
+        }
+        System.out.println("-------------------------------");
+    }
+    
+    public void incrementar(Palabra p){
+        for (Palabra palabra : palabras) {
+            if (palabra.getPalabra().equals(p.getPalabra())) {
+                palabra.setCantidadVeces(palabra.getCantidadVeces() + 1);
+            }
+        }
+    }
+}
---
+++ Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/modelos/Palabra.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejercicio5.ejercicio_5.modelos;
+
+/**
+ *
+ * @author Baltasar
+ */
+public class Palabra {
+    private String palabra;
+    private int cantidadVeces;
+
+    public Palabra(String palabra, int cantidadVeces) {
+        this.palabra = palabra;
+        this.cantidadVeces = cantidadVeces;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public int getCantidadVeces() {
+        return cantidadVeces;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public void setCantidadVeces(int cantidadVeces) {
+        this.cantidadVeces = cantidadVeces;
+    }
+    
+    @Override
+    public String toString(){
+        return "- Palabra: " + "'" + palabra + "'" + " -->" + " Cantidad de veces: " + cantidadVeces;
+    }
+}
