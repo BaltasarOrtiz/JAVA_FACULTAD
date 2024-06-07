c60475872fbe3f6efbcb2e1de49bda8c7801b0b0
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2024-06-06 22:02:11 -0300

    a

---
+++ Ejercicio_5/src/main/java/com/ejercicio5/ejercicio_5/Ejercicio_5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ejercicio5.ejercicio_5;
+
+/**
+ *
+ * @author balta
+ */
+public class Ejercicio_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
