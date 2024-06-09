31a0524570a54ebe8595ee6f8f5637a0777afe45
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2024-06-09 19:51:00 -0300

    Agregados extras

--- parcial2023/src/main/java/com/parcial2023/Finanzas.java
+++ parcial2023/src/main/java/com/parcial2023/Finanzas.java
@@ -1,19 +1,34 @@
 package com.parcial2023;
+import com.parcial2023.interfaces.ITributo;
 import com.parcial2023.modelos.Automotor;
 import com.parcial2023.modelos.Inmueble;
 import java.util.ArrayList;
 
 public class Finanzas {
-    private ArrayList<Inmueble> inmuebles;
-    private ArrayList<Automotor> automotores;
+    //private ArrayList<Inmueble> inmuebles;
+    //private ArrayList<Automotor> automotores;
+    private ArrayList<ITributo> tributos;
 
     public Finanzas() {
-        this.inmuebles = new ArrayList<>();
-        this.automotores = new ArrayList<>();
+        //this.inmuebles = new ArrayList<>();
+        //this.automotores = new ArrayList<>();
+        this.tributos = new ArrayList<>();
     }
 
-    // 1. Implementar el metodo exceptuados()
+    public void insertarTributos(ITributo tributo){
+        tributos.add(tributo);
+        /*
+        if (tributo instanceof Inmueble){
+            inmuebles.add((Inmueble) tributo);
+        } else if (tributo instanceof Automotor){
+            automotores.add((Automotor) tributo);
+        }
+        */
+    }
 
+
+    /* 
+    // 1. Implementar el metodo exceptuados()
     public void exceptuados(){
         System.out.println("Inmuebles exceptuados: ");
         for (Inmueble inmueble : inmuebles){
@@ -25,8 +40,31 @@ public class Finanzas {
             System.out.println(automotor.getPatente());
         }
     }
+    */
 
-    // 2. Codificar totalAutomotores()
+    public void exceptuados2(){
+        for (ITributo tributo : tributos){
+            if (tributo instanceof Inmueble){
+                System.out.println(((Inmueble) tributo).getNumCatastral());
+            } else if (tributo instanceof Automotor){
+                System.out.println(((Automotor) tributo).getPatente());
+            }
+        }
+    }
+
+    // Con streams
+    public void exceptuados3(){
+        tributos.stream()
+                .filter(tributo -> tributo instanceof Inmueble)
+                .forEach(inmueble -> System.out.println(((Inmueble) inmueble).getNumCatastral()));
+
+        tributos.stream()
+                .filter(tributo -> tributo instanceof Automotor)
+                .forEach(automotor -> System.out.println(((Automotor) automotor).getPatente()));
+    }
+    
+    /* 
+    // 2. Codificar totalAutomotores()  
     public void totalAutomotores(){
         int cantAutos2023 = 0;
         for (Automotor automotor : automotores){
@@ -36,7 +74,22 @@ public class Finanzas {
         }
         System.out.println("Cantidad de automotores modelo 2023: " + cantAutos2023);
     }
+    */
 
+    public void totalAutomotores2(){
+        int cantAutos2023 = 0;
+        for (ITributo tributo : tributos){
+            if (tributo instanceof Automotor){
+                Automotor automotor = (Automotor) tributo;
+                if (automotor.getModelo() == 2023){
+                    cantAutos2023++;
+                }
+            }
+        }
+        System.out.println("Cantidad de automotores modelo 2023: " + cantAutos2023);
+    }
+    
+    /* 
     // 3. Codificar megaInmuebles()
     public void megaInmuebles(){
         for (Inmueble inmueble : inmuebles){
@@ -45,5 +98,16 @@ public class Finanzas {
             }
         }
     }
+    */
 
+    public void megaInmuebles2(){
+        for (ITributo tributo : tributos){
+            if (tributo instanceof Inmueble){
+                Inmueble inmueble = (Inmueble) tributo;
+                if (inmueble.impuesto() > 100000){
+                    System.out.println(inmueble.getNumCatastral());
+                }
+            }
+        }
+    }
 }
--- parcial2023/src/main/java/com/parcial2023/Main.java
+++ parcial2023/src/main/java/com/parcial2023/Main.java
@@ -1,10 +1,33 @@
 package com.parcial2023;
+import com.parcial2023.modelos.Automotor;
+import com.parcial2023.modelos.Inmueble;
 
 public class Main {
     public static void main(String[] args) {
+        Automotor auto1 = new Automotor("ABC123", 2023);
+        Automotor auto2 = new Automotor("DEF456", 2022);
+        Automotor auto3 = new Automotor("GHI789", 2023);
+
+        // numero catastral, superficie, zona
+        Inmueble inmueble1 = new Inmueble("123", 100, "urbana");
+        Inmueble inmueble2 = new Inmueble("456", 200, "suburbana");
+        Inmueble inmueble3 = new Inmueble("789", 300, "urbana");
+
         Finanzas f = new Finanzas();
-        f.exceptuados();
-        f.totalAutomotores();
-        f.megaInmuebles();
+        f.insertarTributos(auto1);
+        f.insertarTributos(auto2);
+        f.insertarTributos(auto3);
+        f.insertarTributos(inmueble1);
+        f.insertarTributos(inmueble2);
+        f.insertarTributos(inmueble3);
+
+        System.out.println("Exceptuados: ");
+        f.exceptuados3();
+        System.out.println();
+        System.out.println("Total automotores 2023: ");
+        f.totalAutomotores2();
+        System.out.println();
+        System.out.println("Total inmuebles: ");
+        f.megaInmuebles2();
     }
 }
